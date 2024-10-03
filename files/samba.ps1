67411ef518ddd9d80b79e56cfef37aa63fcb3bf5
devovh <devovh@gmail.com>
2024-10-03 12:13:19 +0200

    * Update...

---
+++ files/samba.ps1
@@ -0,0 +1,6 @@
+﻿Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false
+Set-SmbServerConfiguration -EnableSMB2Protocol $false
+Set-SmbServerConfiguration -AuditSmb1Access $false
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB1 -Type DWORD -Value 0 -Force
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB2 -Type DWORD -Value 0 -Force
