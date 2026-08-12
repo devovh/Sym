9ea869a4dfdd18c84e95d7f2ae27f181086df783
devovh <devovh@gmail.com>
2026-08-12 11:02:53 +0200

    Fix Repair

--- files/fix.bat
+++ files/fix.bat
@@ -1,9 +1,9 @@
+sfc /scannow
 dism /online /cleanup-image /checkhealth
 dism /online /cleanup-image /scanhealth
-dism /online /cleanup-image /restorehealth
 dism /online /cleanup-image /startcomponentcleanup
 dism /online /cleanup-image /startcomponentcleanup /resetbase
-sfc /scannow
+dism /online /cleanup-image /restorehealth
 chkdsk
 chkdsk /scan /forceofflinefix
 pause
